ec9fb20ff6f7c751938b2cbb4ca223008b685210
fym <fym@bitmax.dk>
2019-01-13 10:04:18 +0100

    .sql file cleanup

--- Repository/MySQL/Create table DevidceState.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE `devicestate` (
-  `DeviceGroipId` varchar(50) CHARACTER SET latin1 COLLATE latin1_general_ci DEFAULT NULL,
-  `DeviceId` varchar(50) CHARACTER SET latin1 COLLATE latin1_general_ci DEFAULT NULL,
-  `LastKnownDataValue` varchar(50) CHARACTER SET latin1 COLLATE latin1_general_ci DEFAULT NULL,
-  `LastKnownDataValueDate` datetime DEFAULT NULL,
-  `LastKnownKeepAliveDate` datetime DEFAULT NULL,
-  `LastExecutedKeepaliveCheckDate` datetime DEFAULT NULL,
-  `LastKeepAliveAlert` datetime DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
