e8056b47554cc4c9f8d7f513ed0be24dc97464f3
Frank Mortensen <fym@bitmax.dk>
2020-03-22 23:12:26 +0100

    Config env var read updated

--- Alerting/LanSensor.PollingMonitor/LanSensor.PollingMonitor.Domain/Models/ServiceConfiguration.cs
+++ Alerting/LanSensor.PollingMonitor/LanSensor.PollingMonitor.Domain/Models/ServiceConfiguration.cs
@@ -60,7 +60,7 @@ namespace LanSensor.PollingMonitor.Domain.Models
 
             ApplicationConfiguration.MongoConfiguration = new MongoConfiguration
             {
-                ConnectionString = Environment.GetEnvironmentVariable("mongodbconnectionstring")
+                ConnectionString = Environment.GetEnvironmentVariable("connectionString")
             };
 
             ApplicationConfiguration.SlackConfiguration = new SlackConfiguration
